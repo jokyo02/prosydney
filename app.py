a09b0177ebfc844594185d64e6f3f5636ef32ec9
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2023-11-03 13:42:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def proxy(path):
     data = request.data or request.form or None
     
     # 修改origin头部为https://www.bing.com
-    headers['Host'] = 'sydney.bing.com'
+#    headers['Host'] = 'sydney.bing.com'
     headers['Origin'] = 'https://www.bing.com'
     
     # 发送请求给https://sydney.bing.com，并获取响应
