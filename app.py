3c205bc06f82cc180bdb2680bef776cd20b30993
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2023-11-07 13:54:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ import requests
 # 创建一个Flask应用
 app = Flask(__name__)
 
+# 定义一个函数，生成随机字符串
+# 参数 e 是字符串的长度，默认为 32
+def randomString(e=32):
+    # 定义一个字符串，包含所有可能的字符
+    t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678_-+"
+    # 初始化一个空字符串，用于存储结果
+    n = ""
+    # 循环 e 次，每次从 t 中随机选择一个字符，拼接到 n 中
+    for i in range(e):
+        n += t[getRandomInt(0, len(t))]
+    # 返回 n
+    return n
+
+
 # 定义一个路由，匹配所有的请求
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -18,6 +32,31 @@ def proxy(path):
     # 修改origin头部为https://www.bing.com
     #headers['host'] = 'sydney.bing.com'
     headers['origin'] = 'https://www.bing.com'
+
+    
+
+# 获取请求头中的 Cookie 字段，如果没有则为空字符串
+cookie = request.headers.get('Cookie', '')
+cookies = cookie
+
+# 如果 Cookie 中没有 KievRPSSecAuth 字段，就添加一个随机字符串
+if 'KievRPSSecAuth=' not in cookie:
+    cookies += '; KievRPSSecAuth=' + randomString(512)
+
+# 如果 Cookie 中没有 _RwBf 字段，就添加一个随机字符串
+if '_RwBf=' not in cookie:
+    cookies += '; _RwBf=' + randomString(256)
+
+# 如果 Cookie 中没有 MUID 字段，就添加一个随机字符串
+if 'MUID=' not in cookie:
+    cookies += '; MUID=' + randomString(32)
+
+# 如果 Cookie 中没有 _U 字段，就添加一个随机字符串
+if '_U=' not in cookie:
+    cookies += '; _U=' + randomString(128)
+
+# 设置新的请求头中的 Cookie 字段为 cookies
+newHeaders.set('Cookie', cookies)
     
     # 发送请求给https://sydney.bing.com，并获取响应
     response = requests.request(method, url, headers=headers, data=data)
